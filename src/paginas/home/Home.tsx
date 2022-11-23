6720727fb59d0dcc8027086ebbdbaeeda455b79a
erickbellot12 <erickcesarbellot@gmail.com>
2022-11-23 12:33:51 -0300

    atualizado

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './home.css'
+
+function Home () {
+
+    return (
+       
+        <>
+        <h1 className='titulo'>“Quem combate monstruosidades deve cuidar para que não se torne um monstro. E, se você olhar longamente para um abismo, o abismo também olha para dentro de você.” Friedrich Nietzsche, filósofo alemão (1844-1900)</h1>
+        
+        <div className='container'>
+       
+        <img className='img' src="https://i1.sndcdn.com/artworks-ttgmEvWVcNaGRflz-zMeDFg-t500x500.jpg" alt="imagem imaginaria" />
+
+            
+        </div>
+          
+        </>
+    )
+
+}
+
+export default Home
